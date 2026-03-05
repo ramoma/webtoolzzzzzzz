412fcd7dd4e9bdfc57f6ef8234dd5cbc93fa9367
ramoma <monecraftisgood@gmail.com>
2026-03-05 17:05:10 +0800

    register page is complete

--- Scripts/register_script.js
+++ Scripts/register_script.js
@@ -1,25 +1,53 @@
-document.getElementById("continue").addEventListener("click", function(e){
+document.getElementById("c_submit").addEventListener("click", function(e){
 
     e.preventDefault();
 
-    let username = document.getElementById("username").value;
+    
     let password = document.getElementById("password").value;
     let c_password = document.getElementById("c_password").value;
-
+    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value; 
+    let c_submit = document.getElementById("c_submit").value;
+    
     if(password != "" && c_password != ""){
        if(password == c_password){
-        document.querySelector(".card1").classList.add("hidden");
-        document.querySelector(".card2").classList.remove("hidden");
+            
+            let data = {
+                username: username,
+                c_submit: c_submit,
+                email: email
+
+            };
+
+            let json = JSON.stringify(data);
+
+            fetch("../Api/register.php", {
+                method: "POST",
+                headers: {
+                    "Content-Type" : "application/json"
+                },
+                body: json
+            })
+            .then(res => res.json())
+            .then(data =>{
+
+                if(data.Status === "error"){
+                    console.log(data.Status);
+                    window.alert(data.message);
+                }
+                else{
+                    document.querySelector(".card1").classList.add("hidden");
+                    document.querySelector(".card2").classList.remove("hidden");
+                }
+            }).catch(err=>console.error("something went wrong", err));
+        
         }
         else{
             window.alert("passwords dont match");
         } 
-    }
-    else{
+    }else{
         window.alert("missing input fields");
     }
-    
-
 });
 
 document.getElementById("skip").addEventListener("click", function(e){
@@ -48,9 +76,15 @@ document.getElementById("skip").addEventListener("click", function(e){
     })
     .then(res=>res.json())
     .then(data=>
-        {window.alert("sent verification code");
-            window.open("http://localhost/htmls/webapps_finals/finals%20wewbtools/pages/index.html", "_self");
-            console.log(data.message);
+        {
+            if(data.Status == "error"){
+                console.log(data.message);
+                window.alert(data.message);
+            }
+            else{
+               window.alert("sent verification code");
+                window.open("http://localhost/htmls/webapps_finals/finals%20wewbtools/pages/index.html", "_self"); 
+            }      
     }).catch(err=> console.log("something went wrong", err));
 
 });
@@ -86,9 +120,13 @@ document.getElementById("register_submit").addEventListener("click", function(e)
             body: json
         }).then(res=>res.json())
         .then(data => {
-            window.alert("sent verification code");
-            window.open("http://localhost/htmls/webapps_finals/finals%20wewbtools/pages/index.html", "_self");
-            console.log(data.message);
+
+            if(data.Status === "error"){
+                window.alert("sent verification code");
+                window.open("http://localhost/htmls/webapps_finals/finals%20wewbtools/pages/index.html", "_self");
+                console.log(data.message);
+            }
+            
         })
         .catch(err => console.error("something went wrong", err));
 
