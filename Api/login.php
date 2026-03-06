b4a1b4a525ed155f96f29f09de20f6f77937603e
ramoma <monecraftisgood@gmail.com>
2026-03-06 21:11:14 +0800

    finished back end for login, added user sessions and redirects if user is still in session

--- Api/login.php
+++ Api/login.php
@@ -1,6 +1,14 @@
 <?php
-
     session_start();
+
+    require("connection.php");
+    header("Content-type: application/json");
+
+    $json = file_get_contents("php://input");
+    $data = json_decode($json, true);
+
+    $username = $data["username"] ?? "";
+    $password = $data["password"] ?? "";
 
     if(isset($_SESSION['username'])){
 
@@ -9,12 +17,49 @@
             "message" => "account still logged in"
         ]);
         exit;
+    }
+    if(isset($data["login"])){
+
+        $stmt = $conn->prepare("select count(*) from user_accounts where username = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $stmt->bind_result($check_username);
+        $stmt->fetch();
+        $stmt->close();
+
+        $stmt2 = $conn->prepare("select count(*) from user_accounts where password = ?");
+        $stmt2->bind_param("s", $password);
+        $stmt2->execute();
+        $stmt2->bind_result($check_password);
+        $stmt2->fetch();
+        $stmt2->close();
+
+        if($check_password > 0 && $check_username > 0){
+            
+            $_SESSION["username"] = $username;
+
+            echo json_encode([
+                "Status" => "success",
+                "message" => "logging in" 
+            ]);
+            exit;
+
+            
+        }
+        else{
+            echo json_encode([
+                "Status" => "failed",
+                "message" => "wrong email or password"
+            ]);
+            exit;
+        }
     }else{
         echo json_encode([
-            "Status" => "account_not_logged",
-            "message" => "account is not logged in"
+            "Status" => "not_logged",
+            "message" => "no logged account"
         ]);
         exit;
     }
 
+    
 ?>
